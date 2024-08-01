114aadf9a1251be8b1cd2cc07021b4e7e91e2ad8
Keqing <146825224+XOFef@users.noreply.github.com>
2024-08-01 13:57:02 +0300

    Add files via upload

---
+++ Games/Games.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+
+int main()
+{
+	system("chcp 1251>nul");
+
+	// chapter 1
+
+	// Game over 
+	std::cout << "Game over!" << std::endl;
+
+
+	system("pause>nul");
+	return 0;
+}
