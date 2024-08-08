e40fa08262c8a33b4648c23f977a149f0a3d4f12
Keqing <146825224+XOFef@users.noreply.github.com>
2024-08-08 18:24:20 +0300

    Add files via upload

--- Games/Games.cpp
+++ Games/Games.cpp
@@ -1,4 +1,7 @@
 ﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main()
@@ -8,30 +11,30 @@ int main()
 	// chapter 1
 
 	// 1
-	/*int survivos, lives, score;
-	int i, b, x;*/
+	//int survivos, lives, score;
+	//int i, b, x;
 
-	// 2
-	/*cout << "Seven divided by three is " << 7 / 3 << endl;
-	cout << "Seven divided by three is " << 7.0 / 3 << endl;
-	cout << "Seven divided by three is " << 7.0 / 3.0 << endl;*/
+	//// 2
+	//cout << "Seven divided by three is " << 7 / 3 << endl;
+	//cout << "Seven divided by three is " << 7.0 / 3 << endl;
+	//cout << "Seven divided by three is " << 7.0 / 3.0 << endl;
 
-	// 3 
-	/*int scoreOne = 0, scoreTwo = 0, scoreTree = 0, averageValue = 0;
-	cout << "\n\nEnter the points for the first attempt\n";
-	cin >> scoreOne;
-	cout << "Enter the points for the second attempt\n";
-	cin >> scoreTwo;
-	cout << "Enter the points for the third attempt\n";
-	cin >> scoreTree;
-	averageValue = (scoreOne + scoreTwo + scoreTree) / 3;
-	cout << "The average value is: " << averageValue;*/
+	//// 3 
+	//int scoreOne = 0, scoreTwo = 0, scoreTree = 0, averageValue = 0;
+	//cout << "\n\nEnter the points for the first attempt\n";
+	//cin >> scoreOne;
+	//cout << "Enter the points for the second attempt\n";
+	//cin >> scoreTwo;
+	//cout << "Enter the points for the third attempt\n";
+	//cin >> scoreTree;
+	//averageValue = (scoreOne + scoreTwo + scoreTree) / 3;
+	//cout << "The average value is: " << averageValue;
 
 
 
 	// chapter 2
 
-	//// 1
+	// 1
 	//enum level {Easy, Normal, Hard};
 	//cout << "Difficulty Levels\n\n";
 	//cout << "1 - Easy\n";
@@ -65,23 +68,94 @@ int main()
 	//}
 
 	// 3
-	srand(static_cast<unsigned int>(time(0)));
-	int secretNumber, tries = 0, guessedNumber;
-	cout << "\tWelcome to Guess My Number\n\n";
-	cout << "Enter an odd number: ";
-	cin >> secretNumber;
-	do {
-		guessedNumber = rand() % 100;
-		tries++;
-		cout << "\ntry " << tries << " Number: " << guessedNumber;
-		if (guessedNumber == secretNumber) {
-			cout << "\nPC win";
-			break;
+	//srand(static_cast<unsigned int>(time(0)));
+	//int secretNumber, tries = 0, guessedNumber;
+	//int arr[100];
+	//cout << "\tWelcome to Guess My Number\n\n";
+	//cout << "Enter an odd number: ";
+	//cin >> secretNumber;
+	//do {
+	//	guessedNumber = rand() % 101;
+	//	tries++;
+	//	cout << "\ntry " << tries << " Number: " << guessedNumber << endl;
+	//	if (guessedNumber == secretNumber) {
+	//		cout << "\nPC win";
+	//		break;
+	//	}
+	//} while (true);
+
+
+
+		// chapter 3
+
+	// 1
+	/*srand(static_cast<unsigned int>(time(0)));
+	int point = 0;
+	enum fields { WORD, HINT, NUM_FIELDS };
+	const int NUM_WORDS = 5;
+	const string WORDS[NUM_WORDS][NUM_FIELDS] =
+	{
+		{"wall", "Do you feel you're banging your head against something?"},
+		{"glasses", "These might help you see the answer."},
+		{"labored", "Going slowly, is it?"},
+		{"persistent", "Keep at it."},
+		{"jumble", "It's what the game is all about."}
+	};
+	int choice = (rand() % NUM_WORDS);
+	string theWord = WORDS[choice][WORD];
+	string theHint = WORDS[choice][HINT];
+	string jumble = theWord;
+	int length = jumble.size();
+	for (int i = 0; i < length; ++i)
+	{
+		int index1 = (rand() % length);
+		int index2 = (rand() % length);
+		char temp = jumble[index1];
+		jumble[index1] = jumble[index2];
+		jumble[index2] = temp;
+	}
+
+	cout << "\t\tWelcom to Word Jumble!\n\n";
+	cout << "Unscramble the letters to make a word.\n";
+	cout << "Enter 'hint' fot a hint.\n";
+	cout << "Enter 'quit' to quit the game.\n";
+	cout << "The jumble is: " << jumble;
+	string guess;
+	cout << "\n\nTour guess: ";
+	cin >> guess;
+
+	while ((guess != theWord) && (guess != "quit")) {
+		if (guess == "hint") {
+			cout << theHint;
+			point -= 2;
 		}
-	} while (true);
+		else {
+			cout << "Sorry, that's not it.";
+		}
+		cout << "\n\nYour guess: ";
+		cin >> guess;
+	}
+	if (guess == theWord) {
+		point += theWord.size();
+		cout << "\nThat's it! You guessed it! You scored: " << point << " points\n";
+	}
+	cout << "\nThanks for plaing.";*/
 
+	// 2
+	//string phrase = "Game";
+	//for (int i = 0; i < phrase.size(); ++i) // false - <=
+	//{
+	//	cout << "Character at position " << i+1 << " is: " << phrase[i] << endl; // false - i
+	//}
 
-
+	// 3
+	const int ROWS = 2;
+	const int COLUMNS = 2;
+	char board[ROWS][COLUMNS] =
+	{
+		{'O', 'X', 'O'},
+		{' ', 'X', 'X'}
+	}
 
 
 
